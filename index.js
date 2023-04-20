7ec92e32d8329258d5b0a6663940faf4466db967
Julie C <jucheng925@gmail.com>
2023-04-20 10:44:03 -0400

    sending POST request

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name,email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+         body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`,
+        }),
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        const element = document.createElement("div")
+        element.textContent = `${data.id}`
+        console.log(element)
+        document.body.append(element)
+    })
+    .catch(function (error) {
+        const mess = document.createElement("div")
+        mess.textContent = error.message;
+        console.log(error.message)
+        document.body.append(mess)
+    });
+}
+
